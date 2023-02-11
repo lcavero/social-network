b77b60035fd326773e8cc8f1996bd79253afedbf
luiscavero <luiscaverodeveloper@gmail.com>
2023-02-12 00:20:54 +0100

    testing pre-push hook

---
+++ tests/FooTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FooTest extends TestCase
+{
+
+    public function testFoo(): void
+    {
+        self::assertSame(1, 1);
+    }
+}
