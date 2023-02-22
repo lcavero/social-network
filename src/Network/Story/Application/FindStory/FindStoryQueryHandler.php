4c2d044fc9741774eab3b3a31bca11729287e33a
luiscavero <luiscaverodeveloper@gmail.com>
2023-02-22 23:51:33 +0100

    Improved VO and start Story Aggregate

--- src/Network/Story/Application/FindStory/FindStoryQueryHandler.php
+++ src/Network/Story/Application/FindStory/FindStoryQueryHandler.php
@@ -4,8 +4,6 @@ namespace App\Network\Story\Application\FindStory;
 
 use App\Shared\Domain\Bus\Query\QueryHandlerInterface;
 use App\Shared\Domain\VO\Uuid\Uuid2;
-use App\Shared\Domain\VO\Uuid\UuidValueObject;
-use Symfony\Component\HttpFoundation\Request;
 
 final readonly class FindStoryQueryHandler implements QueryHandlerInterface
 {
@@ -15,11 +13,6 @@ final readonly class FindStoryQueryHandler implements QueryHandlerInterface
 
     public function __invoke(FindStoryQuery $query): FindStoryResult
     {
-        $q = UuidValueObject::generate()->value;
-        $a = Uuid2::fromString($q);
-
-        var_dump(get_class($a));
-
         return FindStoryResult::fromArray([
             'title' => 'dummyTitle',
             'description' => 'dummyDescription'
