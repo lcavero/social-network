91225b0fdb613d50158ca71f5517899b014a9c4d
luiscavero <luiscaverodeveloper@gmail.com>
2023-02-25 22:53:24 +0100

    FindStoryByIdQueryHandlerTest

---
+++ tests/Network/Story/Application/FindStoryById/FindStoryByIdQueryHandlerTest.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\Network\Story\Application\FindStoryById;
+
+use App\Network\Story\Application\FindStoryById\FindStoryByIdQuery;
+use App\Network\Story\Application\FindStoryById\FindStoryByIdQueryHandler;
+use App\Network\Story\Application\FindStoryById\FindStoryByIdResult;
+use App\Network\Story\Domain\Exception\StoryNotFoundException;
+use App\Network\Story\Domain\Search\StoryByIdSearcher;
+use PHPUnit\Framework\TestCase;
+
+final class FindStoryByIdQueryHandlerTest extends TestCase
+{
+    public function testStoryNotFound(): void
+    {
+        $storyId = '0f7429bb-2242-4868-81d5-f140560b317a';
+        $query = FindStoryByIdQuery::create($storyId);
+        $storyByIdSearcherMock = \Mockery::mock(StoryByIdSearcher::class);
+        $storyByIdSearcherMock
+            ->shouldReceive('execute')
+            ->with($storyId)
+            ->once()
+            ->andReturn(null)
+        ;
+        $queryHandler = new FindStoryByIdQueryHandler($storyByIdSearcherMock);
+
+        $this->expectException(StoryNotFoundException::class);
+        ($queryHandler)($query);
+    }
+
+    public function testStoryFound(): void
+    {
+        $storyId = '0f7429bb-2242-4868-81d5-f140560b317a';
+        $query = FindStoryByIdQuery::create($storyId);
+
+        $expectedResult = [
+            'id' => $storyId,
+            'title' => 'Random title',
+            'description' => 'Random description'
+        ];
+
+        $storyByIdSearcherMock = \Mockery::mock(StoryByIdSearcher::class);
+        $storyByIdSearcherMock
+            ->shouldReceive('execute')
+            ->with($storyId)
+            ->once()
+            ->andReturn($expectedResult)
+        ;
+        $queryHandler = new FindStoryByIdQueryHandler($storyByIdSearcherMock);
+
+        $result = ($queryHandler)($query);
+
+        self::assertInstanceOf(FindStoryByIdResult::class, $result);
+        self::assertSame($result->toArray(), $expectedResult);
+    }
+}
